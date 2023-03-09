c4baa34969375a9c03a6f8cfed01c6e62ac47938
Amy Martin <amymartinm97@ucla.edu>
2023-03-09 15:40:16 -0800

    Finished adding routing functions and routes in cart file

--- helpers/cart/cartHelpers.js
+++ helpers/cart/cartHelpers.js
@@ -1,23 +1,32 @@
-const { session } = require('passport');
 const { pool } = require('../../db/server.js');
 
 
 //FIGURE OUT HOW TO ACCESS SESSION ID TO PASS INTO FUNCTION
 
 // Function to retrieve all cart items
-const getAllCartItems = async (sessionId) => {
+const getAllCartItemsQuery = async (sessionID) => {
     try {
         const SQL = 'SELECT * FROM cart_items WHERE session_id=$1'
-        const cart = await pool.query(SQL, [sessionId]);
+        const cart = await pool.query(SQL, [sessionID]);
         return cart.rows;
     } catch (err) {
         console.log(err);
     }
 };
 
+// Callback function to get all cart items route
+const getAllCartItems = async (req, res) => {
+    try {
+        const sessionID = req.sessionID;
+        await getAllCartItemsQuery(sessionID);
+    } catch (err) {
+        console.log(err)
+    }
+
+}
 // Function to retrieve cart item by cart
 
-const getCartItem = async (cartItemId) => {
+const getCartItemQuery = async (cartItemId) => {
     try {
         const SQL = 'SELECT * FROM cart_items WHERE id=$1';
         const cartItem = await pool.query(SQL, [cartItemId]);
@@ -27,34 +36,69 @@ const getCartItem = async (cartItemId) => {
     }
 } 
 
+// Callback function to get cart item route
+const getCartItem = async (req, res) => {
+    try {
+        const {cartItemId} = req.params;
+        await getCartItemQuery(cartItemId);
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
 // Function to add to cart by cart id
 
-const addCartItem = async (sessionId, productId, quantity) => {
+const addCartItemQuery = async (sessionID, productId, quantity) => {
     try {
-        const SQL = 'INSERT INTO cart_items (session_id, product_id, quantity) VALUES ($1, $2, $3)';
-        await pool.query(SQL, [sessionId, productId, quantity]);
+        const SQL = 'INSERT INTO cart_itemO (session_id, product_id, quantity) VALUES ($1, $2, $3)';
+        await pool.query(SQL, [sessionID, productId, quantity]);
     } catch (err) {
         console.log(err);
     }
 }
 
+// Callback function to post cart item route
+
+const addCartItem = async (req, res) => {
+    try {
+        const sessionID = req.sessionID;
+        const {productId, quantity} = req.body;
+        await addCartItemQuery(sessionID, productId, quantity)
+    } catch (err) {
+        console.log(err)
+    }
+};
+
 // Function to remove from cart by cart id and product id
 
-const removeCartItem = async (sessionId, productId) => {
+const deleteCartItemQuery = async (cartItemId, sessionID) => {
     try {
-        const SQL = 'DELETE FROM cart_items WHERE session_id=$1 AND product_id=$2';
-        await pool.query(SQL, [sessionId, productId]);
+        const SQL = 'DELETE FROM cart_items WHERE id=$1 AND session_id=$2';
+        await pool.query(SQL, [cartItemId, sessionID]);
     } catch (err) {
         console.log(err);
+    }
+}
+
+// Callback function to delete cart item route
+
+const deleteCartItem = async (req, res) => {
+    try {
+        const sessionID = req.sessionID;        
+        const {cartItemId} = req.params;
+        await deleteCartItemQuery(cartItemId, sessionID);
+    } catch (err) {
+        console.log(err)
     }
 }
 
 //Function to update cart item quantity 
 
-const updateCartItemQuantity = async (cartItemId, sessionId, productId, quantity) => {
+const updateCartItemQuantityQuery = async (quantity, cartItemId, sessionID) => {
     try {
-        const SQL = 'UPDATE cart_items SET quantity=$1 WHERE session_id=$2 AND product_id=$3'
-        await pool.query(SQL, [quantity, sessionId, productId]);
+        const SQL = 'UPDATE cart_items SET quantity=$1 WHERE id=$2 AND session_id=$3'
+        await pool.query(SQL, [quantity, cartItemId, sessionID]);
         const updatedCartItem = await getCartItem(cartItemId);
         return updatedCartItem
     } catch (err) {
@@ -62,4 +106,17 @@ const updateCartItemQuantity = async (cartItemId, sessionId, productId, quantity
     }
 }
 
-module.exports = { getAllCartItems, getCartItem, addCartItem, removeCartItem, updateCartItemQuantity }
+// Callback function to update cart item quantity route
+
+const updateCartItemQuantity = async (req, res) => {
+    try {
+        const sessionID = req.sessionID;
+        const {cartItemId} = req.params;
+        const { quantity } = req.body
+        await updateCartItemQuantityQuery(quantity, cartItemId, sessionID);
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+module.exports = { getAllCartItems, getCartItem, addCartItem, deleteCartItem, updateCartItemQuantity }
--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,21 @@
-const { pool } = require('../db/server.js')
 const express = require('express');
+const { getCartItem, getAllCartItems, deleteCartItem, updateCartItemQuantity, addCartItem } = require('../helpers/cart/cartHelpers.js');
 const cartRouter = express.Router();
 
-// cartRouter.post()
 
-module.exports = {cartRouter}
+// POST Route to Add Cart Item
+cartRouter.post('/', addCartItem)
+
+// GET Route to Get Cart Item
+cartRouter.get('/:cartItemId', getCartItem)
+
+// GET Route to Get All Cart Items
+cartRouter.get('/', getAllCartItems)
+
+// PUT Route to Update Cart Item Quantity
+cartRouter.put('/:cartItemId', updateCartItemQuantity)
+
+// DELETE Route to Delete Cart Item 
+cartRouter.delete('/:cartItemId', deleteCartItem)
+
+module.exports = {cartRouter};
