01c0566f26ba1f9d29c2f91682c40a9eb7cc1c2c
Amy Martin <amymartinm97@ucla.edu>
2023-04-10 15:24:15 -0700

    Added changes in order to interact with backend api and changed web icon

--- frontend/ecommerceapp/src/app/components/register/Register.js
+++ frontend/ecommerceapp/src/app/components/register/Register.js
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from "react-router-dom"
 
 export const Register = () => {
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [telephone, setTelephone] = useState();
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confirmationPassword, setConfirmationPassword] = useState();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Entered handlesubmit')
+        if (password !== confirmationPassword) {
+            throw new Error('Passwords Do Not Match')
+        } else {
+            console.log('HandleSubmit: ' + firstName)
+            const requestOptions = {
+                method: 'POST', 
+                body: JSON.stringify({
+                    firstName,
+                    lastName,
+                    telephone,
+                    username,
+                    email,
+                    password
+                })
+            };
+            fetch('http://localHost:3000/register', requestOptions)
+                .then(response => console.log(response));
+        }
+    }
+
     return (
-        <div className="register-page">
-            <form>
+        <div className="registration">
+            <form className="registration-form" onSubmit={handleSubmit}>
+                <div className="registration-container">
+                    <div className="first-name entry">
+                        <label>First Name:</label>
+                        <input type="text" placeholder="" name="first-name" onChange={e => setFirstName(e.target.value)}></input>
+                    </div>
+                    <div className="last-name entry">
+                        <label>Last Name: </label>
+                        <input type="text" placeholder="" name="last-name" onChange={e => setLastName(e.target.value)}></input>
+                    </div>
+                    <div className="telephone entry">
+                        <label>Phone Number:</label>
+                        <input type="text" placeholder="" name="telephone" onChange={e => {setTelephone(e.target.value)}}></input>
+                    </div>
+                    <div className="username entry">
+                        <label>Username:</label>
+                        <input type="text" placeholder="" name="username" onChange={e => setUsername(e.target.value)}></input>
+                    </div>
+                    <div className="email entry">
+                        <label>Email:</label>
+                        <input type="text" placeholder="" name="email" onChange={e => setEmail(e.target.value)}></input>
+                    </div>
+                    <div className="password entry">
+                        <label>Password:</label>
+                        <input type="password" placeholder="" name="password" onChange={e => setPassword(e.target.value)}></input>
+                    </div>
+                    <div className="confirmation-password entry">
+                        <label>Confirm Password:</label>
+                        <input type="password" placeholder="" name="confirmation-password" onChange={e => setConfirmationPassword(e.target.value)}></input>
+                    </div>
+                    <button>Submit</button>
+                </div>
                 
             </form>
         </div>
