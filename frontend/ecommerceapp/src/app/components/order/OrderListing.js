5418937898a7620ffef2fce51913fa17b8d94eb0
Amy Martin <amymartinm97@ucla.edu>
2024-01-03 22:55:41 -0500

    Rework promise handling within OrderListing while updating datatype pushed to orderList

--- frontend/ecommerceapp/src/app/components/order/OrderListing.js
+++ frontend/ecommerceapp/src/app/components/order/OrderListing.js
@@ -1,52 +1,59 @@
-import React, { useEffect } from "react"; 
+import React, { useEffect, useState } from "react"; 
 import { ItemTile } from "../cart/ItemTile";
 import { Loading } from "../miscellaneous/Loading";
 import { serverAddress } from "../../App";
 
 export const OrderListing = (props) => {
-    const {orderDetails, orderDetailsStatus} =  props;
-    const {order_items: orderItems} = orderDetails
-    let orderList = []
+    const { orderDetails, orderDetailsStatus } = props;
+    const { order_items: orderItems } = orderDetails;
+
+    const [orderList, setOrderList] = useState([]);
+    
     useEffect(() => {
-        if (orderItems.length > 0) {
-            mapToOrderList(orderItems);
-        }
-    }, [orderItems])
-    const fetchOrderItemDetails = async (desc) => {
-        try {
-            const options = {
-                method: 'GET',
-                mode: 'cors',
-                credentials: 'include',
-                headers: {
-                    Accept: 'application/json',
-                    "Content-Type": "application/json"
+        const fetchOrderItemDetails = async (desc) => {
+            try {
+                const options = {
+                    method: 'GET',
+                    mode: 'cors',
+                    credentials: 'include',
+                    headers: {
+                        Accept: 'application/json',
+                        "Content-Type": "application/json"
                     }
-                }
-            const descWithUnderscores = desc.replace(/ /g, '_')
-            const url = `${serverAddress}/product/desc/${descWithUnderscores}`;
-            
-            const response = await fetch(url, options);
-            const data = response.json();
-            return data
-        } catch (e) {
-            throw e
+                };
+
+                const descWithUnderscores = desc.replace(/ /g, '_');
+                const url = `${serverAddress}/product/desc/${descWithUnderscores}`;
+                
+                const response = await fetch(url, options);
+                const data = await response.json();
+                return data.product;
+            } catch (e) {
+                throw e;
+            }
+        };
+
+        const mapToOrderList = async () => {
+            const promises = orderItems.map(async item => {
+                return await fetchOrderItemDetails(item.description);
+            });
+
+            const result = await Promise.all(promises);
+            setOrderList(result);
+        };
+
+        if (orderItems.length > 0) {
+            mapToOrderList();
         }
-    }
-    const mapToOrderList = (orderItems) => {
-            orderItems.map(async item => {
-                const product = await fetchOrderItemDetails(item.description)
-                console.log(product)
-            })
-        
-    }
+    }, [orderItems]);
+
     return (
         <div className="order-listing-container"> 
             <h3>Order #: {orderDetails.id}</h3>
-            {orderList.length > 0 ? orderList.map(item => {
-                return <ItemTile type='order-item' item={item} key={item.id}/>
-            }): <Loading />}
+            {orderList.length > 0 ? orderList.map(item => (
+                <ItemTile type='order-item' item={item} key={item.id} />
+            )) : <Loading />}
             <h3>Total: {orderDetails.total}</h3>
         </div>
-    )
-} 
+    );
+};
